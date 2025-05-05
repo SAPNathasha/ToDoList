ceedc4499dfd5d29c521dcd9ad85f5467b08c5bc
SAPNathasha <131260315+SAPNathasha@users.noreply.github.com>
2025-05-05 11:17:39 +0530

    Check the date if weekend or not

--- app.js
+++ app.js
@@ -4,9 +4,18 @@ const bodyParser = require("body-parser");
 const app = express();
 
 app.get("/" , function(req,res){
-    res.send("Hello");
+
+    var today = new Date();
+    if(today.getDate() === 6 || today.getDate() === 0){
+        res.send("Woohoo! It's weekend!");
+    }else{
+        res.send("Ooh! I have to work!");
+    };
 });
 
+
+
+
 app.listen(3000, function(){
-    console.log("Server started on port 3000");
+    console.log("Running port 3000");
 });
