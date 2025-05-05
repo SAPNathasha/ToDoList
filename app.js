99f61278241544f55798e0a74f24f014a8097d54
SAPNathasha <131260315+SAPNathasha@users.noreply.github.com>
2025-05-05 21:33:04 +0530

    Use EJS javascript in html pages

--- app.js
+++ app.js
@@ -3,38 +3,34 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.get("/" , function(req,res){
+app.get("/", function (req, res) {
+  var today = new Date();
+  var currentDay = today.getDay();
+  var day = "";
 
-    var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
+  if (currentDay === 1) {
+    day = "Monday";
+  } else if (currentDay === 2) {
+    day = "Tuesday";
+  } else if (currentDay === 3) {
+    day = "Wednesday";
+  } else if (currentDay === 4) {
+    day = "Thursday";
+  } else if (currentDay === 5) {
+    day = "Friday";
+  } else if (currentDay === 6) {
+    day = "Saturday";
+  } else if (currentDay === 0) {
+    day = "Sunday";
+  } else {
+    console.log("Wrong date");
+  }
 
-    if(currentDay === 1){
-        day = "Monday";
-    }else if(currentDay === 2){
-        day = "Tuesday";
-    }else if(currentDay === 3){
-        day = "Wednesday";
-    }else if(currentDay === 4){
-        day = "Thursday";
-    }else if(currentDay === 5){
-        day = "Friday";
-    }else if(currentDay === 6){
-        day = "Saturday";
-    }else if(currentDay === 0){
-        day = "Sunday";
-    }else{
-        console.log("Wrong date");
-    }
-
-    res.render("list" , {kindOfDay: day});
-
+  res.render("list", { kindOfDay: day });
 });
 
-
-
-app.listen(3000, function(){
-    console.log("Running port 3000");
+app.listen(3000, function () {
+  console.log("Running port 3000");
 });
